652904ab834af554010a9cf5d5b055fee47e1029
Silvia Neves <siltatiana.sn@gmail.com>
2022-09-29 14:50:42 +0100

    creates all the components files

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header_socials">
+      <a href="https://www.linkedin.com/in/silvia-neves-silvianeves">Linkedin</a>
+      <a href="https://github.com/Sil-Tatiana">GitHub</a>
+    </div>
+  )
+}
+
+export default HeaderSocials
