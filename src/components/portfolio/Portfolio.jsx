286cdd249eb1733b2cb6370fa567be3119e4acc7
Silvia Neves <siltatiana.sn@gmail.com>
2022-10-02 16:25:31 +0100

    creates HTML for Portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './portfolio.css'
+import PLANTREAT from "../assets/plantreat.png";
+import BARKAWAY from "../assets/barkaway.png";
 
 const Portfolio = () => {
   return (
@@ -10,18 +12,18 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         <article className="portfolio__item">
           <div className="portfolio__itme-image">
-            <img src="" alt="" />
+            <img src={PLANTREAT} alt="Plant app" />
             <h3>Portfolio Title</h3>
-            <a href="" className='btn'>Github</a>
-            <a href="" className='btn btn-primary' target='_blank' >Live Demo</a>
+            <a href="https://github.com/felipesferreiradev/plantreat" target='_blank' className='btn'>Github</a>
+            <a href="http://www.plantreat.lol/" className='btn btn-primary' target='_blank' >Live Demo</a>
           </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__itme-image">
-          <img src="" alt="" />
+          <img src={BARKAWAY} alt="Dog walker website" />
             <h3>Portfolio Title</h3>
-            <a href="" className='btn'>Github</a>
-            <a href="" className='btn btn-primary' target='_blank' >Live Demo</a>
+            <a href="https://github.com/felipesferreiradev/BARK_AWAY" target='_blank' className='btn'>Github</a>
+            <a href="https://bark-away.herokuapp.com/" className='btn btn-primary' target='_blank' >Live Demo</a>
           </div>
         </article>
       </div>
