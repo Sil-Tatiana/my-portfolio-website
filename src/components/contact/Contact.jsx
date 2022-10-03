c5dfae86a6eace166c7ac87acc6278107bfc8779
Silvia Neves <siltatiana.sn@gmail.com>
2022-10-03 19:34:52 +0100

    creates HTML structure for Contacts  section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,22 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container container__contact">
+        <div className="contact__options">
+          <article className="contact__option">
+            <h4>Email</h4>
+            <h5>siltatiana.sn@gmail.com</h5>
+            <a href="malito:siltatiana.sn@gmail.com">Email me</a>
+          </article>
+        </div>
+
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
