cf8916ab1bec807f85b0bc66d65c7406baa679b1
Silvia Neves <siltatiana.sn@gmail.com>
2022-10-03 19:47:17 +0100

    adds form to Contacts  section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './contact.css'
+import { HiOutlineMail } from 'react-icons/hi';
 
 const Contact = () => {
   return (
@@ -10,13 +11,18 @@ const Contact = () => {
       <div className="container container__contact">
         <div className="contact__options">
           <article className="contact__option">
+            < HiOutlineMail/>
             <h4>Email</h4>
             <h5>siltatiana.sn@gmail.com</h5>
-            <a href="malito:siltatiana.sn@gmail.com">Email me</a>
+            <a href="mailto:siltatiana.sn@gmail.com">Email me</a>
           </article>
         </div>
 
-        <form action=""></form>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your email' required />
+          <textarea name="message" id="textarea_contact" rows="7" required></textarea>
+        </form>
       </div>
     </section>
   )
